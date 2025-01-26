edbcd751818216e5d2eadb1dc6d6acd15efcd6b4
JyotiradeetiBagal <139766154+JyotiradeetiBagal@users.noreply.github.com>
2025-01-26 19:05:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ myPromise()
             // Filter out odd numbers
             setTimeout(() => {
                 const evenNumbers = numbers.filter(num => num % 2 === 0);
-                updateOutput("output", evenNumbers.join(", "));
+                updateOutput("output", evenNumbers.join(","));
                 resolve(evenNumbers);
             }, 1000);
         });
@@ -31,7 +31,7 @@ myPromise()
             // Multiply even numbers by 2
             setTimeout(() => {
                 const multipliedNumbers = evenNumbers.map(num => num * 2);
-                updateOutput("output", multipliedNumbers.join(", "));
+                updateOutput("output", multipliedNumbers.join(","));
                 resolve(multipliedNumbers);
             }, 2000);
         });
