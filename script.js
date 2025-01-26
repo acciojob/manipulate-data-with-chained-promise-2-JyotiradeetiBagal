4a1426b087014f7202275ac0460fb7c81dc5050e
JyotiradeetiBagal <139766154+JyotiradeetiBagal@users.noreply.github.com>
2025-01-26 18:42:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 //your JS code here. If required.
+
+function myPromise() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve([1, 2, 3, 4]);
+        }, 3000);
+    });
+}
+
+function updateOutput(id, content) {
+    const outputElement = document.getElementById(id);
+    if (outputElement) {
+        outputElement.textContent = content;
+    }
+}
+
+myPromise()
+    .then(numbers => {
+        return new Promise((resolve) => {
+            const evenNumbers = numbers.filter(num => num % 2 === 0);
+            setTimeout(() => {
+                updateOutput("output", evenNumbers.join(", "));
+                resolve(evenNumbers);
+            }, 1000);
+        });
+    })
+    .then(evenNumbers => {
+        return new Promise((resolve) => {
+            const multipliedNumbers = evenNumbers.map(num => num * 2);
+            setTimeout(() => {
+                updateOutput("output", multipliedNumbers.join(", "));
+                resolve(multipliedNumbers);
+            }, 2000);
+        });
+    })
+    .catch(err => {
+        console.error("An error occurred:", err);
+    });
