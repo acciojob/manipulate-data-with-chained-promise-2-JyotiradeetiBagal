30301d45a4a9ded9330bd4a0a04a3c44fd6bf822
JyotiradeetiBagal <139766154+JyotiradeetiBagal@users.noreply.github.com>
2025-01-26 19:00:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,8 +18,9 @@ function updateOutput(id, content) {
 myPromise()
     .then(numbers => {
         return new Promise((resolve) => {
-            const evenNumbers = numbers.filter(num => num % 2 === 0);
+            // Filter out odd numbers
             setTimeout(() => {
+                const evenNumbers = numbers.filter(num => num % 2 === 0);
                 updateOutput("output", evenNumbers.join(", "));
                 resolve(evenNumbers);
             }, 1000);
@@ -27,8 +28,9 @@ myPromise()
     })
     .then(evenNumbers => {
         return new Promise((resolve) => {
-            const multipliedNumbers = evenNumbers.map(num => num * 2);
+            // Multiply even numbers by 2
             setTimeout(() => {
+                const multipliedNumbers = evenNumbers.map(num => num * 2);
                 updateOutput("output", multipliedNumbers.join(", "));
                 resolve(multipliedNumbers);
             }, 2000);
